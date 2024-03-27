8b5cd3c6d7b582bd4311a54304a8016d91b76535
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-03-27 12:14:40 +0100

    add sources

--- cldfbench_barlownumeralsystems.py
+++ cldfbench_barlownumeralsystems.py
@@ -1,11 +1,18 @@
 import pathlib
+import re
 
 from cldfbench import CLDFSpec, Dataset as BaseDataset
+from pybtex.database import parse_string
 
 
 def normalise(id_):
     id_ = id_.replace(' ', '-')
     return id_
+
+
+def clean_sources(sources_field):
+    sources = (s.strip() for s in sources_field.split(','))
+    return [re.sub(r'\s*\([^)]*\)', '', s) for s in sources if s]
 
 
 class Dataset(BaseDataset):
@@ -48,6 +55,18 @@ class Dataset(BaseDataset):
                 'barlow-austronesian-papuan-numeral-systems.csv', dicts=True))
         raw_data = [row for row in raw_data if row]
 
+        parameter_table = list(
+            self.etc_dir.read_csv('parameters.csv', dicts=True))
+        code_table = {
+            (row['Parameter_ID'], row['Original_Name']): row
+            for row in self.etc_dir.read_csv('codes.csv', dicts=True)}
+
+        sources = parse_string(self.raw_dir.read('sources.bib'), 'bibtex')
+
+        language_sources = {
+            row['Glottocode']: clean_sources(row.get('Datasets') or '')
+            for row in raw_data}
+
         glottocodes = {row['Glottocode'] for row in raw_data}
         language_table = [
             {
@@ -58,14 +77,9 @@ class Dataset(BaseDataset):
                 'Longitude': lang.longitude,
                 'Glottocode': lang.id,
                 'ISO639P3code': lang.iso,
+                'Source': language_sources[lang.id],
             }
             for lang in args.glottolog.api.languoids(ids=glottocodes)]
-
-        parameter_table = list(
-            self.etc_dir.read_csv('parameters.csv', dicts=True))
-        code_table = {
-            (row['Parameter_ID'], row['Original_Name']): row
-            for row in self.etc_dir.read_csv('codes.csv', dicts=True)}
 
         for parameter in parameter_table:
             if parameter.get('Closed_Value_Set') == 'yes':
@@ -99,7 +113,9 @@ class Dataset(BaseDataset):
             for parameter in parameter_table
             if (value := row.get(parameter['Original_Name']))]
 
-        args.writer.cldf.add_component('LanguageTable')
+        args.writer.cldf.add_component(
+            'LanguageTable',
+            'http://cldf.clld.org/v1.0/terms.rdf#source')
         args.writer.cldf.add_component('ParameterTable')
         args.writer.cldf.add_component('CodeTable', 'Map_Icon')
 
@@ -107,3 +123,5 @@ class Dataset(BaseDataset):
         args.writer.objects['ParameterTable'] = parameter_table
         args.writer.objects['CodeTable'] = code_table.values()
         args.writer.objects['ValueTable'] = value_table
+
+        args.writer.cldf.add_sources(sources)
