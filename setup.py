a09e0a8cc3bdfc532a7337fd315b2cad3a1043cf
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 11:58:07 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_barlownumeralsystems',
-    py_modules=['cldfbench_barlownumeralsystems'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'barlownumeralsystems=cldfbench_barlownumeralsystems:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
